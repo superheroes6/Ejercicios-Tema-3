18a79c0a1468c33535a60e336ecfb69867b1a7bb
santibox888@gmail.com <santibox888@gmail.com>
2025-04-03 19:43:58 +0200

    cambio4

---
+++ Ejercicio_4/4.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def restar(po1, po2):
+    resta = np.polysub(po1, po2)
+    return resta
+
+def dividir(po1, po2):
+    division = np.polydiv(po1, po2)
+    return division
+
+def eliminar(polinomio, termino):
+    polinomio = list(polinomio)
+    if termino in polinomio:
+        polinomio.remove(termino)
+    return polinomio
+
+def existe(polinomio, termino):
+    exist = termino in polinomio
+    return exist
+
+p1 = [3, 2, 1]
+p2 = [1, 0, -1]
+
+print("Resta de polinomios:", restar(p1, p2))
+
+cociente, residuo = dividir(p1, p2)
+print("División de polinomios: Cociente:", cociente, "Residuo:", residuo)
+
+termino_a_eliminar = 2
+print("Polinomio después de eliminar el término", termino_a_eliminar, ":", eliminar(p1, termino_a_eliminar))
+
+termino_a_buscar = 3
+print("¿Existe el término", termino_a_buscar, "en el polinomio?", existe(p1, termino_a_buscar))
